96bcf401877c931fc1101beac0368c236ba02c2b
shun197590 <a8156297@gmail.com>
2026-09-02 16:16:23 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "live-interpreter-pwa10-v1";
+const CACHE_NAME = "live-interpreter-pwa10-1-v1";
 
 const ASSETS = [
   "./",
@@ -10,38 +10,44 @@ const ASSETS = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
       )
-    )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+  if(event.request.method !== "GET") return;
 
   event.respondWith(
     fetch(event.request)
       .then(response => {
         const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, copy));
+
         return response;
       })
       .catch(() =>
-        caches.match(event.request).then(response =>
-          response || caches.match("./index.html")
-        )
+        caches.match(event.request)
+          .then(response =>
+            response || caches.match("./index.html")
+          )
       )
   );
 });
